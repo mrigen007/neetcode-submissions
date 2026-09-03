82d45fa4cf506b4cc1d0ea7de14b50222d376912
Mrigendrakumar <127305577+mrigen007@users.noreply.github.com>
2026-09-03 17:35:12 +0530

    Add: valid-palindrome-ii - submission-6

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-6.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+
+            if (s.charAt(l) != s.charAt(r)) {
+
+                // left character skip
+                int i = l + 1;
+                int j = r;
+
+                while (i < j && s.charAt(i) == s.charAt(j)) {
+                    i++;
+                    j--;
+                }
+
+                if (i >= j)
+                    return true;
+
+                // right character skip
+                i = l;
+                j = r - 1;
+
+                while (i < j && s.charAt(i) == s.charAt(j)) {
+                    i++;
+                    j--;
+                }
+
+                return i >= j;
+            }
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
